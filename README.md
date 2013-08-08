6aba8765456e525cd417dbadb377bf450f9b279d
Joseph <joseph.dillon.522@gmail.com>
2013-08-07 19:48:53 -0700

    Basic 2
    form submission works with validation and error messages

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ RoR assignment while attending Coding Dojo, Mountain View, CA
 Basic 0: added Users and Posts controllers and associating views/layouts
 
 Basic 1: added User model, added info in DB, displayed data on users/index
+
+Basic 2: enabled the form submission and validation response
--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
   	@user_data = User.all 
   end
 
+  def create
+	  	@new_user = User.new(params[:user])
+
+	  	respond_to do |format|
+			if @new_user.save
+				format.html { redirect_to users_path, notice: 'Post was successfully created.' }
+				format.json { render json: @new_user, status: :created, location: @user }
+			else
+				format.html { render action: "new" }
+				format.json { render json: @new_user.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
   def new
+  	@new_user = User.new
   end
+
 end
