a0f71a8a264869e892e6b3b41b7cc0c5fc3fd317
Joseph <joseph.dillon.522@gmail.com>
2013-08-08 13:14:24 -0700

    Basic 3
    Added the ability to edit, view, and delete a user. Converted all forms
    to simple forms. Got some neat XP on editing foundation.

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,24 +1,67 @@
 class UsersController < ApplicationController
+  
   def index
-  	@user_data = User.all 
+  	@user = User.all 
+  end
+
+  def new
+		@user = User.new
+
+		respond_to do |format|
+			format.html # new.html.erb
+			format.json { render json: @user }
+		end
   end
 
   def create
-	  	@new_user = User.new(params[:user])
+	  	@user = User.new(params[:user])
 
 	  	respond_to do |format|
-			if @new_user.save
+			if @user.save
 				format.html { redirect_to users_path, notice: 'Post was successfully created.' }
-				format.json { render json: @new_user, status: :created, location: @user }
+				format.json { render json: @user, status: :created, location: @user }
 			else
 				format.html { render action: "new" }
-				format.json { render json: @new_user.errors, status: :unprocessable_entity }
+				format.json { render json: @user.errors, status: :unprocessable_entity }
 			end
 		end
 	end
 
-  def new
-  	@new_user = User.new
+	def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
   end
+
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+  		@user = User.find(params[:id])
+	  	respond_to do |format|	
+			if @user.update_attributes(params[:user])
+		        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
+		        format.json { head :no_content }
+		    else
+		        format.html { render action: "edit" }
+		        format.json { render json: @user.errors, status: :unprocessable_entity }
+		    end
+		end
+  end
+
+	def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_path }
+      format.json { head :no_content }
+    end
+  end
+  
 
 end
